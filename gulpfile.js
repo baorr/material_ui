a87aa4febac34e24e714cb11b7f2843335518018
baobao <baoranran@wanda.cn>
2018-04-23 14:07:41 +0800

    fixed build command

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,8 @@
  
 const gulp = require('gulp');
 const sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
 
-gulp.task('pret', function () {
+gulp.task('sass', function () {
   return gulp.src('./sass/app/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css/app'));
