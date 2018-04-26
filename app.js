48f9cf18e206c54566138c5d862fc17ec212d9c0
baobao <baoranran@wanda.cn>
2018-04-26 11:08:14 +0800

    code reconstruct

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
+
+router.get("/demo",function(req,res){
+  res.sendFile(__dirname + "/demo.html");
+});
+
+app.use(router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
