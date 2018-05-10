50a31e78cd61ec88eecf8d54226b00b6ddfb1e72
baobao <baoranran@wanda.cn>
2018-05-10 15:59:11 +0800

    ui component v1.0.0 release

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,28 @@
  
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const path = require('path');
+
+const ROOT = path.resolve(__dirname, '../');
+const project = path.resolve(ROOT,'baorr.github.io');
+const mudule = path.resolve(project, 'css/ui');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/app/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css/app'));
+});
+
+gulp.task('ui', function () {
+  return gulp.src('./sass/ui/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/ui'));
+});
+
+
+gulp.task('baorr', function () {
+  return gulp.src('./sass/ui/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(mudule));
 });
